3e9dca90f48fdfba2fab584415fe6c6016f208d3
Zidane Zilame <dilamoffical@gmail.com>
2023-10-16 12:08:13 +0700

    Done Column & Cards

--- src/theme.js
+++ src/theme.js
@@ -1,59 +1,65 @@
-import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
+const APP_BAR_HEIGHT = "58px";
+const BOARD_BAR_HEIGHT = "60px";
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT}  - ${BOARD_BAR_HEIGHT})`;
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: '58px',
-    boardBarHeight: '60px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
   },
   colorSchemes: {
     light: {},
-    dark: {}
+    dark: {},
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: {
         body: {
-          '*::-webkit-scrollbar': {
-            width: '8px',
-            height: '8px'
+          "*::-webkit-scrollbar": {
+            width: "8px",
+            height: "8px",
           },
-          '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#dcdde1',
-            borderRadius: '8px'
+          "*::-webkit-scrollbar-thumb": {
+            backgroundColor: "#dcdde1",
+            borderRadius: "8px",
           },
-          '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: 'white'
-          }
-        }
-      }
+          "*::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "white",
+          },
+        },
+      },
     },
     MuiButton: {
       styleOverrides: {
         root: {
-          textTransform: 'none',
-          borderWidth: '0.5px',
-          '&:hover': { borderColor: '#bdc3c7 !important' }
-        }
-      }
+          textTransform: "none",
+          borderWidth: "0.5px",
+          "&:hover": { borderColor: "#bdc3c7 !important" },
+        },
+      },
     },
-    MuiInputLabel: {
+    MuiTypography: {
       styleOverrides: {
-        root: { fontSize: '0.875rem' }
-      }
+        root: { 
+          '&.MuiTypography-body1': {fontSize: '0.875rem'}
+         },
+      },
     },
     MuiOutlinedInput: {
       styleOverrides: {
         root: {
-          fontSize: '0.875rem',
-          '& fieldset': { borderWidth: '1px !important' },
-          '&:hover fieldset': { borderColor: '#bdc3c7 !important' },
-          '&:Mui-focused fieldset': { borderColor: '#bdc3c7 !important' }
-        }
-      }
-    }
-  }
+          fontSize: "0.875rem",
+          "& fieldset": { borderWidth: "1px !important" },
+          "&:hover fieldset": { borderColor: "#bdc3c7 !important" },
+          "&:Mui-focused fieldset": { borderColor: "#bdc3c7 !important" },
+        },
+      },
+    },
+  },
   // ...other properties
-})
+});
 
-export default theme
+export default theme;
