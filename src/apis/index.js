16475847d484c269f640011ff73259871bce155d
boybun0154 <huyhoang201555@gmail.com>
2023-11-08 15:46:57 +0700

    DndColApi

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { API_ROOT } from '~/utils/constants'
 
+export const updateBoard = async (boardId, data) => {
+  const response = await axios.put(`${API_ROOT}/v1/boards/${boardId}`, data)
+  return response.data
+}
+
 export const fetchBoardDetailsAPI = async (boardId) => {
   const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
   return response.data
