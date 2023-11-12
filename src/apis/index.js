e7cdcd89734e31babcf7d91cb1c61147847ac016
lilinkw <kjlinh098@gmail.com>
2023-11-12 10:43:00 +0700

    Add Board List Page

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,7 +4,6 @@ import { auth } from "./auth";
 import { auth as authHelper } from "../helpers/index";
 
 export const createNewBoard = async (data) => {
-  console.log("data", data);
   const response = await axios.post(`${API_ROOT}/v1/boards`, {
     title: data.title,
     description: data.description,
@@ -65,6 +64,20 @@ export const updateCard = async (id, data) => {
 export const findUsernameById = async (id) => {
   const response = await axios.get(`${API_ROOT}/v1/auth/${id}`)
   return response.data
+}
+
+/**
+ * 
+ * @param {string} id
+ * @returns {Promise<string[]>} boards
+ */
+export const getBoardsIdByUserId = async (id) => {
+  const response = await axios.post(
+    `${API_ROOT}/v1/boards/user/`,
+    { id },
+    authHelper.getHeader()
+  );
+  return response.data;
 }
 
 export { auth };
